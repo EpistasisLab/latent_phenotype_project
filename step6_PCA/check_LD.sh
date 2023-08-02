6d26aa57d3b04278939551c468614ea6fb5b3bbd
greggj2016 <greggj2016@gmail.com>
2023-08-01 20:39:44 -0400

    final updates

--- step6_PCA/check_LD.sh
+++ step6_PCA/check_LD.sh
@@ -4,6 +4,6 @@
 #BSUB -e check_LD.error
 #BSUB -R "rusage[mem=20000MB]"
 #BSUB -M 20000MB
-module load plink/1.90Beta
+module load plink/1.90Beta6.18
 
 plink --memory 15000 --bfile UKB_samples_unrelated_pruned --r --keep-allele-order --ld-window-kb 200 --ld-window 1000000 --with-freqs --ld-window-r2 0 --out UKB_samples_unrelated_pruned
